f76af1e7759fdde975156f323d57ecd6cbf87a4f
Radek Scheibinger <rscheibinger@ebay.com>
2015-12-31 11:59:51 -0800

    added note on installin oh-my-zsh

--- modules/people/manifests/scheibinger/applications.pp
+++ modules/people/manifests/scheibinger/applications.pp
@@ -2,6 +2,9 @@ class people::scheibinger::applications {
   package { 'evernote': provider => 'brewcask' }
   package { 'tig': }
   package { 'maven': }
+  package { 'tmux': }
+
+  # todo: intall  oh-my-zsh https://github.com/robbyrussell/oh-my-zsh
 
   include spotify
   include iterm2::stable
